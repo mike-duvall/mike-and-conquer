b810e2368aecfbadd5af1e4835e592c6161b642e
mike-duvall <mike.a.duvall@gmail.com>
2022-11-20 09:08:42 -0600

    Cleanup

--- mike_and_conquer_monogame/rest/controller/UIMouseCursorStateController.cs
+++ mike_and_conquer_monogame/rest/controller/UIMouseCursorStateController.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using mike_and_conquer_monogame.gameview;
-using mike_and_conquer_monogame.main;
-using mike_and_conquer_monogame.rest.domain;
-using mike_and_conquer_simulation.rest.controller;
 
 namespace mike_and_conquer_monogame.rest.controller
 {
     [ApiController]
-    //    [Route("[controller]")]
-
     [Route("ui/query/mouseCursor")]
 
 
@@ -29,18 +24,10 @@ namespace mike_and_conquer_monogame.rest.controller
         public ActionResult Get()
         {
 
+            // TODO Should this be retrieved via event on game loop?
             string cursorState = GameWorldView.instance.gameCursor.StateAsString;
             return new OkObjectResult(cursorState);
 
-            // UnitView unitView = MikeAndConquerGame.instance.GetUnitViewByIdByEvent(unitId);
-            //
-            //
-            // RestUnit restUnit = new RestUnit();
-            //
-            // restUnit.UnitId = unitView.UnitId;
-            // restUnit.Selected = unitView.Selected;
-            //
-            // return new OkObjectResult(restUnit);
         }
 
 
