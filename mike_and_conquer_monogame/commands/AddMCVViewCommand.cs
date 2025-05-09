bc94ae2f6fe1bdc58dfcbaeaa1e22152a73c17e3
mike-duvall <mike.a.duvall@gmail.com>
2025-05-09 15:54:31 -0500

    PR cleanup

--- mike_and_conquer_monogame/commands/AddMCVViewCommand.cs
+++ mike_and_conquer_monogame/commands/AddMCVViewCommand.cs
@@ -6,21 +6,6 @@ namespace mike_and_conquer_monogame.commands;
 
 public class AddMCVViewCommand : AsyncViewCommand
 {
-    // private int unitId;
-    // private int x;
-    // private int y;
-    //
-    // public AddMCVViewCommand(int unitId, int x, int y)
-    // {
-    //     this.unitId = unitId;
-    //     this.x = x;
-    //     this.y = y;
-    // }
-    //
-    // protected override void ProcessImpl()
-    // {
-    //     MikeAndConquerGame.instance.AddMCVView(unitId, x, y);
-    // }
     private static readonly ILogger Logger = Log.ForContext<AddMinigunnerViewCommand>();
 
 
